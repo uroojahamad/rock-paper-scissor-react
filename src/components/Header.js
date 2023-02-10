8dac8bfc15364da7e7219acd90bff7753882715e
Urooj Ahmad <uroojahamad786@gmail.com>
2023-02-10 22:03:19 +0530

    header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,15 +3,13 @@ import '../css/style.css';
 
 const Header = () => {
   return (
-    <header>
-      <nav>
+    <header> 
         <div className="logo-image">
           <img src={require("../images/Rock-Paper-Scissors.png")} />
         </div>
         <div className="main-heading">
           <h1>Rock Paper Scissor</h1>
         </div>
-      </nav>
     </header>
   );
 };
