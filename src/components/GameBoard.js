18ee46ac04e1ef77c9a1f55a0581af6377bd85f7
Urooj Ahmad <uroojahamad786@gmail.com>
2023-02-10 22:03:40 +0530

    game board component

--- src/components/GameBoard.js
+++ src/components/GameBoard.js
@@ -1,57 +1,58 @@
-import React , {useState} from 'react';
+import React , {useState, useEffect} from 'react';
 
 const GameBoard = () => {
 
     const [userSelected, setUserSelected] = useState(null);
     const [computerSelected, setComputerSelected] = useState(null);
+    const [winner, setWinner] = useState(null);
     const [userScore, setUserScore] = useState(0);
     const [computerScore, setComputerScore] = useState(0);
+
+    const resultText = {
+        "user" : `You chose ${userSelected}. You Won! 🥳🥳`,
+        "computer" : `Computer chose ${computerSelected}. You Lose! 😥😥`,
+        "draw" : `It's a Draw. You both selected same thing.`
+    }
 
     function getComputerChoice(){
         const choices = ['rock', 'paper', 'scissors']
         const randomNumber = Math.floor(Math.random() * 3);
         return choices[randomNumber];
     }
-    
-      function startGame(value) {
+
+    function startGame(value) {
         setUserSelected(value);
-        setComputerSelected(getComputerChoice());
-        
+        const computerChoice = getComputerChoice();
+        setComputerSelected(computerChoice);
     }
 
-    function selectWinner(){
-        const criteria = `${userSelected}-${computerSelected}`;
-        let result = null;
-        switch(criteria){
-            case "paper-rock":
-            case "scissors-paper":
-            case "rock-scissors":
-                result = "user";
-                setUserScore((previousState) => previousState + 1);
-                break;
-            case "rock-paper":
-            case "paper-scissors":
-            case "scissors-rock":
-                result = "computer";
-                setComputerScore((previousState) => previousState + 1);
-                break;
-            case "rock-rock":
-            case "paper-paper":
-            case "scissors-scissors":
-                result = "draw";
-                break;
-            default:
-                result = null;
+    useEffect(() => {
+        if (computerSelected !== null && userSelected !== null)
+            selectWinner();
+    }, [computerSelected, userSelected]);
+    
+    let result = null;
+    function selectWinner(){        
+        if(userSelected === computerSelected){
+            result = 'draw';
         }
-        return result;
+
+        const criteria = `${userSelected}-${computerSelected}`;
+        if (['paper-rock', 'scissors-paper', 'rock-scissors'].indexOf(criteria) === -1){
+            result = 'computer';
+        }
+        result = 'user';
+        setWinner(result);
     }
 
-
-    const resultText = {
-        "user" : `You choose ${userSelected}. You Won! 🥳🥳`,
-        "computer" : `Computer chooses ${computerSelected}. You Lose! 😥😥`,
-        "draw" : `It's a Draw. You both selected same thing.`
-    }
+    useEffect(() => {
+        if (winner === 'user'){
+            setUserScore(prevScore => prevScore + 1);
+        }
+        if (winner === 'computer'){
+            setComputerScore(prevScore => prevScore + 1);
+        }
+    }, [winner]);
 
     return (
         <div className="gameBoard">
@@ -59,38 +60,37 @@ const GameBoard = () => {
                 <h2>Pick One</h2>
             </div>
             <div className="choices">
-                <button className="choice" value="rock" onClick={(e) => startGame(e.target.alt)}>
+                <button className="choice" value="rock" onClick={() => startGame('rock')}>
                     <img src={require("../images/rock.png")} alt="rock" />
                 </button>
-                <button className="choice" value="paper" onClick={(e) => startGame(e.target.alt)}>
+                <button className="choice" value="paper" onClick={() => startGame('paper')}>
                     <img src={require("../images/paper.png")} alt="paper" />
                 </button>
-                <button className="choice" value="scissors" onClick={(e) => startGame(e.target.alt)}>
+                <button className="choice" value="scissors" onClick={() => startGame('scissors')}>
                     <img src={require("../images/scissors.png")} alt="scissors" />
                 </button>
             </div>
             <div className="result">
                 <div className="user">
-                    <h3>You</h3>
+                    <h2>You</h2>
                     <div className="result-image">
                        {userSelected && <img className="user-selected" src={require(`../images/${userSelected}.png`)} />}
                     </div>
                 </div>
                 <div className="verus">
-                    <h3>VS</h3>
+                    <p>VS</p>
                 </div>
                 <div className="computer">
-                    <h3>Computer</h3>
+                    <h2>Computer</h2>
                     <div className="result-image">
                        {computerSelected && <img className="computer-selected" src={require(`../images/${computerSelected}.png`)} />}
                     </div>
                 </div>
             </div>
             <div className="winner">
-                <div className="winner-text user-win hidden">
-                    <span>                     
-                    </span>
-                </div>
+                {winner && <div className={`winner-text ${winner}-win`}>
+                    <span>{resultText[result]}</span>
+                </div>}
                 <div className="scores">
                     <span className="user-score">{userScore}</span>
                     <span>-</span>
